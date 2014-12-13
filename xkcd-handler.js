597fc714515a200e61b68377f8e92a1d3629e01b
Andy Tu <andytu@alumni.ubc.ca>
2014-12-13 12:04:10 -0800

    Add functionality of navigating to different comics as per the original site

--- xkcd-handler.js
+++ xkcd-handler.js
@@ -2,9 +2,19 @@
 var oReq = new XMLHttpRequest();
 // Variables to store comic numbers to keep track of which is displayed.
 var displayedComic = null;
-var currentComic = null;
+var latestComic = null;
 
-// Eventlisteners for buttons TODO
+// Eventlisteners for buttons
+var first = document.getElementById("first");
+var prev = document.getElementById("prev");
+var rand = document.getElementById("random");
+var next = document.getElementById("next");
+var last = document.getElementById("last");
+first.onload = first.addEventListener("click", getFirst, false);
+prev.onload = prev.addEventListener("click", getPrevious, false);
+rand.onload = rand.addEventListener("click", getRandom, false);
+next.onload = next.addEventListener("click", getNext, false);
+last.onload = last.addEventListener("click", getLast, false);
 
 // Initialize to most recent comic
 // Specify json response type since xkcd stores comic info in json
@@ -26,7 +36,7 @@ function reqListener() {
 
 //Set most recent comic and run reqListener()
 function setup() {
-  currentComic = this.response.num;
+  latestComic = this.response.num;
   var listener = reqListener.bind(this);
   listener();
 }
@@ -38,4 +48,33 @@ function setComic(url) {
   Req.onload = reqListener;
   Req.open("GET", url, true);
   Req.send();
+}
+
+// Navigation functions
+// Get first comic
+function getFirst() {
+  setComic("http://xkcd.com/1/info.0.json");
+}
+
+// Get previous comic
+function getPrevious() {
+  if ( displayedComic - 1 >= 1 )
+    setComic("http://xkcd.com/"+ (displayedComic - 1) + "/info.0.json");
+}
+
+// Get random comic
+function getRandom() {
+  var randComic = Math.floor((Math.random() * latestComic) + 1);
+  setComic("http://xkcd.com/"+ randComic + "/info.0.json");
+}
+
+// Get next comic
+function getNext() {
+  if(displayedComic + 1 <= latestComic)
+    setComic("http://xkcd.com/"+ (displayedComic + 1) + "/info.0.json");
+}
+
+// Get most recent comic
+function getLast() {
+  setComic("http://xkcd.com/info.0.json");
 }
